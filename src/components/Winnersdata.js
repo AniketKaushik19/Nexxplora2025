96528363cfc665bbcb974fd0f5127e8dbe2a254f
AniketKaushik19 <aniketsskaushik@gmail.com>
2025-09-22 21:55:52 +0530

    winner list update

--- src/components/Winnersdata.js
+++ src/components/Winnersdata.js
@@ -12,9 +12,18 @@ export const eventWinnersData = {
     eventName: "🏆 Ad-mad Show",
     totalParticipants: 0,
     winners: [
-      { position: "First 🥇", name: "", rollNumber: "" },
-      { position: "Second 🥈", name: "", rollNumber: "" },
-      { position: "Third 🥉", name: "", rollNumber: "" }
+      { position: "First 🥇", name: "Vijya Verma", rollNumber: "2404850100192" },
+      { position: "First 🥇", name: "Siddhart Jaiswal", rollNumber: "2404850100175" },
+      { position: "First 🥇", name: "Kishan Paswan", rollNumber: "2404850100090" },
+      { position: "Second 🥈", name: "preeti Sharma", rollNumber: "Not Available" },
+      { position: "Second 🥈", name: "Nidhi Sharma", rollNumber: "Not Available" },
+      { position: "Third 🥉", name: "Pranjali Singh", rollNumber: "2404850100126" },
+      { position: "Third 🥉", name: "Vaishnavi", rollNumber: "2404850100187" },
+      { position: "Third 🥉", name: "Supriya Singh", rollNumber: "2404850100180" },
+      { position: "Third 🥉", name: "Saloni Singh", rollNumber: "2404850100154" },
+      { position: "Third 🥉", name: "Rupesh Soni", rollNumber: "2404850100144" },
+      { position: "Third 🥉", name: "Mohd. Ateek", rollNumber: "2404850100106" },
+      { position: "Third 🥉", name: "Vanya Srivastava", rollNumber: "2404850100191" },
     ]
   },
   "3": {
@@ -31,7 +40,7 @@ export const eventWinnersData = {
     totalParticipants: 120,
     winners: [
       { position: "First 🥇", name: "Irfan Ali", rollNumber: "2304851540028" },
-      { position: "Second 🥈", name: "Aryan Singh", rollNumber: "" },
+      { position: "Second 🥈", name: "Aryan Singh", rollNumber: "Lateral" },
       { position: "Third 🥉", name: "Amrit Gupta", rollNumber: "2304850130010" }
     ]
   },
@@ -39,45 +48,70 @@ export const eventWinnersData = {
     eventName: "🏆 Coding and Debugging",
     totalParticipants: 0,
     winners: [
-      { position: "First 🥇", name: "", rollNumber: "" },
-      { position: "Second 🥈", name: "", rollNumber: "" },
-      { position: "Third 🥉", name: "", rollNumber: "" }
+      { position: "First 🥇", name: "Vansh Maurya", rollNumber: "2304851530066" },
+      { position: "Second 🥈", name: "Piyush Vishwakarma", rollNumber: "2404850100123" },
+      { position: "Third 🥉", name: "Rajnandani Sultania", rollNumber: "2404850100132" }
     ]
   },
   "6": {
     eventName: "🏆 Lan Gaming / Online Gaming",
     totalParticipants: 0,
     winners: [
-      { position: "First 🥇", name: "", rollNumber: "" },
-      { position: "Second 🥈", name: "", rollNumber: "" },
-      { position: "Third 🥉", name: "", rollNumber: "" }
+      { position: "BGMI First 🥇", name: "Anand Chaurasiya", rollNumber: "2204850100040" },
+      { position: "BGMI First 🥇", name: "Aditya Singh", rollNumber: "2204850100016" },
+      { position: "BGMI First 🥇", name: "Abhishek Yadav", rollNumber: "2204850100008" },
+      { position: "BGMI First 🥇", name: "Alok Singh", rollNumber: "2204850100030" },
+      { position: "BGMI Second 🥈", name: "Ajit Kumar", rollNumber: "Not Available" },
+      { position: "BGMI Second 🥈", name: "Raj Kishor", rollNumber: "Not Available" },
+      { position: "BGMI Second 🥈", name: "Santosh Pandey", rollNumber: "Not Available" },
+      { position: "BGMI Second 🥈", name: "Ravi Gupta", rollNumber: "Not Available" },
+    
+
+      { position: "Free Fire First 🥇", name: "Yash Rai", rollNumber: "2304850100128" },
+      { position: "Free Fire First 🥇", name: "Om ji", rollNumber: "2304850100119" },
+      { position: "Free Fire First 🥇", name: "Avesh", rollNumber: "23048501300061" },
+      { position: "Free Fire First 🥇", name: "Shivam", rollNumber: "2404851530060" },
+      { position: "Free Fire Second 🥈", name: "Abhishek Verma", rollNumber: "Not Available" },
+      { position: "Free Fire Second 🥈", name: "Adarsh Pandey", rollNumber: "Not Available" },
+      { position: "Free Fire Second 🥈", name: "Samar Pandey", rollNumber: "Not Available" },
+      { position: "Free Fire Second 🥈", name: "Ansh Chauhan", rollNumber: "Not Available" },
+      { position: "Free Fire Third 🥉", name: "Lavkush", rollNumber: "2304850130038" },
+      { position: "Free Fire Third 🥉", name: "Jay Kishan Maurya", rollNumber: "2204851530020" },
+      { position: "Free Fire Third 🥉", name: "Ajay Kushwaha", rollNumber: "230485031008" },
+      { position: "Free Fire Third 🥉", name: "Ankush sharma", rollNumber: "2304850130013" },
     ]
   },
   "7": {
     eventName: "🏆 Mock Interview",
     totalParticipants: 0,
     winners: [
-      { position: "First 🥇", name: "", rollNumber: "" },
-      { position: "Second 🥈", name: "", rollNumber: "" },
-      { position: "Third 🥉", name: "", rollNumber: "" }
+      { position: "First 🥇", name: "Siddhant Jaiswal", rollNumber: "2404850100175" },
+      { position: "Second 🥈", name: "Aniket Kaushik", rollNumber: "2204850100042" },
+      { position: "Third 🥉", name: "Aman Pal", rollNumber: "2304851530007" }
     ]
   },
   "8": {
     eventName: "🏆 Poster Making",
     totalParticipants: 0,
     winners: [
-      { position: "First 🥇", name: "", rollNumber: "" },
-      { position: "Second 🥈", name: "", rollNumber: "" },
-      { position: "Third 🥉", name: "", rollNumber: "" }
+      { position: "First 🥇", name: "Sanskriti", rollNumber: "2304851530053" },
+      { position: "Second 🥈", name: "Anvisha", rollNumber: "240485010027" },
+      { position: "Third 🥉", name: "Diksha", rollNumber: "2304851530027" }
     ]
   },
   "9": {
     eventName: "🏆 PPT/Project Presentation",
     totalParticipants: 0,
     winners: [
-      { position: "First 🥇", name: "", rollNumber: "" },
-      { position: "Second 🥈", name: "", rollNumber: "" },
-      { position: "Third 🥉", name: "", rollNumber: "" }
+      { position: "Project First 🥇", name: "Aniket Kaushik", rollNumber: "2204850100042" },
+      { position: "Project Second 🥈", name: "Sonia Sharma", rollNumber: "2204850100170" },
+      { position: "Project Third 🥉", name: "Ankit Kumar Sankar", rollNumber: "2404850130013" },
+      { position: "Project Third 🥉", name: "Ritesh Kumar", rollNumber: "2404850130047" },
+      { position: "Project Third 🥉", name: "Mausami Verma", rollNumber: "24048501300.." },
+      { position: "PPT First 🥇", name: "Nidhi Sharma", rollNumber: "240485010036" },
+      { position: "PPT Second 🥈", name: "Nitin Kumar", rollNumber: "230485130043" },
+      { position: "PPT Third 🥉", name: "Sidhant Jaiswal", rollNumber: "2404850100175" },
+      { position: "PPT Third 🥉", name: "Pranjal Sharma", rollNumber: "24048501001526" },
     ]
   },
   "10": {
@@ -85,17 +119,17 @@ export const eventWinnersData = {
     totalParticipants: 120,
     winners: [
       { position: "First 🥇", name: "Shivam Verma", rollNumber: "2304851540057" },
-      { position: "Second 🥈", name: "Sameer Patel", rollNumber: "24048550100156" },
-      { position: "Third 🥉", name: "Ayushi Maurya", rollNumber: "24048550100065" }
+      { position: "Second 🥈", name: "Sameer Patel", rollNumber: "2404850100156" },
+      { position: "Third 🥉", name: "Ayushi Maurya", rollNumber: "2404850100065" }
     ]
   },
   "11": {
     eventName: "🏆 SR Got Talent",
     totalParticipants: 0,
     winners: [
-      { position: "First 🥇", name: "", rollNumber: "" },
-      { position: "Second 🥈", name: "", rollNumber: "" },
-      { position: "Third 🥉", name: "", rollNumber: "" }
+      { position: "First 🥇", name: "Ashutosh Sharma", rollNumber: "Not Available" },
+      { position: "Second 🥈", name: "Siddharth Singh", rollNumber: "Not Available" },
+      { position: "Third 🥉", name: "Aditi Shukla", rollNumber: "Not Available" }
     ]
   },
   "14": {
@@ -104,7 +138,7 @@ export const eventWinnersData = {
     winners: [
       { position: "First 🥇", name: "Vaishnavi Verma", rollNumber: "2204850100181" },
       { position: "Second 🥈", name: "Saurabh Chauhan", rollNumber: "2404850130053" },
-      { position: "Second 🥈", name: "Khushi Tiwari", rollNumber: "" },
+      { position: "Second 🥈", name: "Khushi Tiwari", rollNumber: "Not Available" },
       { position: "Third 🥉", name: "Arnav Bhatnagar", rollNumber: "2304850130014" },
       { position: "Third 🥉", name: "Siddhant", rollNumber: "2404850100175" }
     ]
@@ -115,35 +149,38 @@ export const eventWinnersData = {
     winners: [
       { position: "First 🥇", name: "Swayam Gupta", rollNumber: "2404851530065" },
       { position: "Second 🥈", name: "Sameer Patel", rollNumber: "2404850100156" },
-      { position: "Third 🥉", name: "Amit Prajapati", rollNumber: "" }
+      { position: "Third 🥉", name: "Amit Prajapati", rollNumber: "Not Available" }
     ]
   },
   "16": {
     eventName: "🏆 Capture the Flag",
     totalParticipants: 0,
     winners: [
-      { position: "First 🥇", name: "", rollNumber: "" },
-      { position: "Second 🥈", name: "", rollNumber: "" },
-      { position: "Third 🥉", name: "", rollNumber: "" }
+      { position: "First 🥇", name: "Ishan Mishra", rollNumber: "2304850100084" },
+      { position: "Second 🥈", name: "Ayushman", rollNumber: "2404850100066" },
+      { position: "Second 🥈", name: "Raj Soni", rollNumber: "2304851540043" },
+      { position: "Third 🥉", name: "Aradhya Sahu", rollNumber: "2404850130001" },
+      { position: "Third 🥉", name: "Rupesh Pandey", rollNumber: "2304851530052" },
+      { position: "Third 🥉", name: "Deep Dwivedi", rollNumber: "2304850100069" }
     ]
   },
   "17": {
     eventName: "🏆 Content Writing",
     totalParticipants: 0,
     winners: [
-      { position: "First 🥇", name: "", rollNumber: "" },
-      { position: "Second 🥈", name: "", rollNumber: "" },
-      { position: "Third 🥉", name: "", rollNumber: "" }
+      { position: "First 🥇", name: "Rohit Kumar", rollNumber: "2304851530048" },
+      { position: "Second 🥈", name: "Dhirendra ", rollNumber: "2304851530031" },
+      { position: "Third 🥉", name: "Rajesh Kumar Dubey", rollNumber: "2404850100174" }
     ]
   },
   "18": {
     eventName: "🏆 Group Discussion",
     totalParticipants: 120,
     winners: [
-      { position: "First 🥇", name: "Aprajit", rollNumber: "" },
-      { position: "Second 🥈", name: "Ahaan", rollNumber: "" },
+      { position: "First 🥇", name: "Aprajit", rollNumber: "Not Available" },
+      { position: "Second 🥈", name: "Ahaan", rollNumber: "Not Available" },
       { position: "Second 🥈", name: "Preeti Sharma", rollNumber: "2404851540042" },
-      { position: "Third 🥉", name: "Shreejal", rollNumber: "" }
+      { position: "Third 🥉", name: "Shreejal", rollNumber: "Not Available" }
     ]
   },
   "22": {
@@ -159,18 +196,24 @@ export const eventWinnersData = {
     eventName: "🏆 Pictionary",
     totalParticipants: 0,
     winners: [
-      { position: "First 🥇", name: "", rollNumber: "" },
-      { position: "Second 🥈", name: "", rollNumber: "" },
-      { position: "Third 🥉", name: "", rollNumber: "" }
+      { position: "First 🥇", name: "Anushka Rajput", rollNumber: "2404850540026" },
+      { position: "First 🥇", name: "Aprajit Mishra", rollNumber: "2404850540028" },
+      { position: "First 🥇", name: "Naincy Pandey", rollNumber: "2404850540077" },
+      { position: "Second 🥈", name: "Divya Verma", rollNumber: "2404850100069" },
+      { position: "Second 🥈", name: "Janvi Tripathi", rollNumber: "2404850100084" },
+      { position: "Second 🥈", name: "Kartik Rastogi", rollNumber: "2404850100087" },
+      { position: "Third 🥉", name: "Aditya Rajput", rollNumber: "2404850310003" },
+      { position: "Third 🥉", name: "Naincy Yadav", rollNumber: "2404850130039" },
+      { position: "Third 🥉", name: "Yogesh Yadav", rollNumber: "2404850310058" },
     ]
   },
   "20": {
     eventName: "🏆 Hackathon",
     totalParticipants: 0,
     winners: [
-      { position: "First 🥇", name: "", rollNumber: "" },
-      { position: "Second 🥈", name: "", rollNumber: "" },
-      { position: "Third 🥉", name: "", rollNumber: "" }
+      { position: "First 🥇", name: "Not Available", rollNumber: "Not Available" },
+      { position: "Second 🥈", name: "Not Available", rollNumber: "Not Available" },
+      { position: "Third 🥉", name: "Not Available", rollNumber: "Not Available" }
     ]
   },
   "23": {
@@ -186,28 +229,28 @@ export const eventWinnersData = {
     eventName: "🏆 Logo Design",
     totalParticipants: 0,
     winners: [
-      { position: "First 🥇", name: "", rollNumber: "" },
-      { position: "Second 🥈", name: "", rollNumber: "" },
-      { position: "Third 🥉", name: "", rollNumber: "" }
+      { position: "First 🥇", name: "Shagun Mitra", rollNumber: "2404081060052" },
+      { position: "Second 🥈", name: "Nishant Kumar", rollNumber: "Not Available" },
+      { position: "Third 🥉", name: "Shushant Ke. Singh", rollNumber: "2304850100191" }
     ]
   },
   "25": {
     eventName: "🏆 Reverse Coding",
     totalParticipants: 0,
     winners: [
-      { position: "First 🥇", name: "", rollNumber: "" },
-      { position: "Second 🥈", name: "", rollNumber: "" },
-      { position: "Third 🥉", name: "", rollNumber: "" }
+      { position: "First 🥇", name: "Aviral Pratap Singh", rollNumber: "2404850100061" },
+      { position: "Second 🥈", name: "Anirudh Gupta", rollNumber: "2404851540020" },
+      { position: "Third 🥉", name: "Prateek Gautam", rollNumber: "Not Available" }
     ]
   },
   "26": {
     eventName: "🏆 Tech Meme War",
     totalParticipants: 120,
     winners: [
-      { position: "First 🥇", name: "Rachit Pandey", rollNumber: "" },
-      { position: "First 🥇", name: "Subhrat", rollNumber: "" },
+      { position: "First 🥇", name: "Rachit Pandey", rollNumber: "2304850100139" },
+      { position: "First 🥇", name: "Subhrat", rollNumber: "2304850100192" },
       { position: "Second 🥈", name: "Shyam Anuj Dubey", rollNumber: "2404850100174" },
-      { position: "Third 🥉", name: "Jyoti Prabha", rollNumber: "" },
+      { position: "Third 🥉", name: "Jyoti Prabha", rollNumber: "2404851540028" },
       { position: "Third 🥉", name: "Khushi", rollNumber: "2404851520033" }
     ]
   },
@@ -226,8 +269,14 @@ export const eventWinnersData = {
     totalParticipants: 120,
     winners: [
       { position: "First 🥇", name: "Mohammad Ateek", rollNumber: "2404850100106" },
-      { position: "Second 🥈", name: "Aditya Chauhan", rollNumber: "" },
-      { position: "Third 🥉", name: "Piyush Kumar", rollNumber: "" }
+      { position: "First 🥇", name: "Vanya Srivastava", rollNumber: "2404850100191" },
+      { position: "First 🥇", name: "Supriya Singh", rollNumber: "2404850100180" },
+      { position: "Second 🥈", name: "Aditya Chauhan", rollNumber: "2404850100022" },
+      { position: "Second 🥈", name: "Sameer Patel", rollNumber: "2404850100155" },
+      { position: "Second 🥈", name: "Abhinadan Shukla", rollNumber: "2404850100007" },
+      { position: "Third 🥉", name: "Aviral Pratap Singh", rollNumber: "2404850100061" },
+      { position: "Third 🥉", name: "Piyush Kumar", rollNumber: "2404851520045" },
+      { position: "Third 🥉", name: "Aman Kumar Verma", rollNumber: "2404851530005" }
     ]
   }
 };
